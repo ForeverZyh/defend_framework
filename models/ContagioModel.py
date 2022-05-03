d3ef99dfefd6d1dfd8fcf6d3c6a656a94dfc50f6
Yuhao Zhang <yuhao.zhang@wisc.edu>
2022-05-03 14:26:12 -0500

    Add contagio dataset (half done)

---
+++ models/ContagioModel.py
@@ -0,0 +1,34 @@
+from sklearn.ensemble import RandomForestClassifier
+
+from models.Model import Model
+
+
+class ContagioModel(Model):
+    def __init__(self, n_features, n_classes, lr=1e-3):
+        super(ContagioModel, self).__init__(n_features, n_classes, lr)
+
+    def build_model(self):
+        model = RandomForestClassifier(
+            n_estimators=1000,  # Used by PDFrate
+            criterion="gini",
+            max_depth=None,
+            min_samples_split=2,
+            min_samples_leaf=1,
+            bootstrap=True,
+            oob_score=False,
+            n_jobs=-1,  # Run in parallel
+            random_state=None,
+            verbose=0
+        )
+
+        return model
+
+    def fit(self, X, y, batch_size, epochs):
+        self.model.fit(X, y)
+
+    def evaluate(self, x_test, y_test):
+        prediction_label = self.model.predict(x_test)
+        score = self.model.score(x_test, y_test)
+        print('Test accuracy:', score)
+
+        return prediction_label
